ed830ba25354597896dbce08fdb10fec7f3bc5c2
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2024-07-31 20:48:36 -0300

    feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useFetch } from '../../Hooks/useFetch.jsx'
 
 const Feed = () => {
-    return <>
-        FEED
-    </>
+    const { data, error, request } = useFetch()
+
+    React.useEffect(async () => {
+        await request('URL')
+    }, [])
+
+    if(error) return <p>{error}</p>
+    return <section>
+        {data.map(post => {
+            return <div>
+                <h2>{post.title}</h2>
+                <p>{post.description}</p>
+            </div>
+        })}
+    </section>
 }
 
 export default Feed
--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -3,14 +3,12 @@ import React from "react";
 const useFetch = () => {
     const [data, setData] = React.useState(null)
     const [error, setError] = React.useState(null)
-    const [loading, setLoading] = React.useState(false)
 
     const request = React.useCallback(async (url, options) => {
         let json
         let response
         try{
             setError(null)
-            setLoading(true)
             response = await fetch(url, options)
             json = await response.json()
             if(!response.ok) throw new Error(json.message)
@@ -19,7 +17,6 @@ const useFetch = () => {
             setError(err.message)
         }finally{
             setData(json)
-            setLoading(false)
             return {response, json}
         }
     }, [])
@@ -28,7 +25,6 @@ const useFetch = () => {
     return {
         data,
         error,
-        loading,
         request
     }
 }
